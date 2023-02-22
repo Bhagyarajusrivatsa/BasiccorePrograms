cd21e577e72403e3dcb18e27d953732da3a7ab35
Bhagya Raju <bhagyarajusrivatsa12@gmail.com>
2023-02-22 15:18:34 +0530

    BasiccorePrograms

---
+++ BasiccorePrograms/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace BasiccorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                Console.WriteLine("Enter the number of times to flip the coin:");
+                int numFlips = Convert.ToInt32(Console.ReadLine());
+
+                if (numFlips <= 0)
+                {
+                    Console.WriteLine("Please enter a positive integer.");
+                    return;
+                }
+
+                int numHeads = 0;
+                int numTails = 0;
+                Random rnd = new Random();
+
+                for (int i = 0; i < numFlips; i++)
+                {
+                    int flip = rnd.Next(0, 2);
+                    if (flip == 0)
+                    {
+                        numHeads++;
+                    }
+                    else
+                    {
+                        numTails++;
+                    }
+                }
+
+                double percentageHeads = ((double)numHeads / numFlips) * 100;
+                double percentageTails = ((double)numTails / numFlips) * 100;
+
+                Console.WriteLine("Percentage of heads: " + percentageHeads.ToString("0.00") + "%");
+                Console.WriteLine("Percentage of tails: " + percentageTails.ToString("0.00") + "%");
+            }
+        }
+    }
+}
